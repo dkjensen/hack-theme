8146361df884304410d111068d4b7deca9cdc5e4
David Jensen <david@dkjensen.com>
2021-07-20 17:56:21 -0400

    fix: index.php title if no posts present

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ get_header();
 
 <div class="section section-index-hero purple-background-color">
 	<div class="wrap">
-		<h1><?php echo esc_html( get_post_type_object( get_post_type() )->label ); ?></h1>
+		<h1><?php echo wp_kses_post( is_post_type_archive() ? post_type_archive_title( '', false ) : get_the_archive_title() ); ?></h1>
 		<p><?php esc_html_e( 'The Indigitous team is here to help you kick off your Hackathon. Please reach out to us if you need specific guidance from our team.', 'hack' ); ?></p>
 		<p><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'contact' ) ) ); ?>"><?php esc_html_e( 'Contact Us', 'hack' ); ?></a></p>
 	</div>
