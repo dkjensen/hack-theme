e03aa75abf15efcd83686aea29171c9b7f337429
Ricardo Bautista <rickyhaswifi@gmail.com>
2021-10-14 12:05:12 -0600

    fix: updates to video sub pagee

--- templates/template-locations-video.php
+++ templates/template-locations-video.php
@@ -19,12 +19,19 @@ if ( have_posts() ) :
 						<div class="column five-sixths offset-1">
 							<h1>
 								<?php
-								echo esc_html__( 'Video Submission', 'hack' );
+								echo esc_html__( 'Best Location Video', 'hack' );
 								?>
 							</h1>
-							<p><?php esc_html_e( 'Use the form below to submit your location video.', 'hack' ); ?></p>
+							<p><?php esc_html_e( 'With over 60 cities taking part of #HACK2021 we want to showcase the variety of locations. To represent your location, please send us a video of you doing anything, literally anything, and your location will be featured in a fun video after #HACK2021.', 'hack' ); ?></p>
+			  <ul class="basic-list">
+				<li><?php esc_html_e( 'Most creative videos will get special mention at the Global Celebration.' ); ?></li>
+				<li><?php esc_html_e( 'The video can feature an individual or a whole team.' ); ?></li>
+				<li><?php esc_html_e( 'This is the sound-track we are using: ' ); ?><a href="https://www.epidemicsound.com/track/Z0PXwNiDAA/" class="text-color--primary" target="_blank" style="color: #ee5028;">New Dawn Is Rising</a>.</li>
+				<li><?php esc_html_e( 'We need to be able to download your video.' ); ?></li>
+			  </ul>
 			  <div class="form-card">
 			  <?php the_content(); ?>
+		<p class="form-disclaimer"><?php esc_html_e( 'By submitting your video you approve its use for Inditigitous/#HACK marketing materials.' ); ?></p>
 			  </div>
 						</div>
 					</div>
